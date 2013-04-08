d8a21f94bd1be47bd86fd81a43cff29c514f3fce
Chris <christmonstah@googlemail.com>
2013-04-08 08:26:21 +0200

    init

---
+++ studimarkr/src/de/home/uni/studimarkr/client/HomeViewImpl.java
@@ -0,0 +1,9 @@
+package de.home.uni.studimarkr.client;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class HomeViewImpl extends Composite implements HomeView{
+	
+	
+
+}
---
+++ studimarkr/src/de/home/uni/studimarkr/client/LoginCss.java
@@ -0,0 +1,9 @@
+package de.home.uni.studimarkr.client;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface LoginCss extends CssResource {
+	
+	@ClassName("loginContainer")
+	String getLoginContainer();
+}
